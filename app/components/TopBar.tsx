f24c6b8dbec2f920ac97010bb874077d1f8b6b70
kallolghosal <kghosal@gmail.com>
2025-12-03 12:09:29 +0530

    Golite first commit

---
+++ app/components/TopBar.tsx
@@ -0,0 +1,14 @@
+
+
+export default function TopBar() {
+  return (
+    <div className="w-full h-12 bg-gray-100 flex items-center justify-center text-gray-600">
+      <div className="max-w-2xl w-full px-4">
+        Personal | Business
+      </div>
+      <div className="max-w-6xl w-full px-4 text-right">
+        Coverage Map | BYOD | Travel Plans | Top-Up | International Calls | Device Protection | Help &amp; Support
+      </div>
+    </div>
+  )
+}
