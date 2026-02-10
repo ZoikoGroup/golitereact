036bb023cd0275d0737f0b5320046f2dff7545cc
Anowar Sadat <anowar.vista@gmail.com>
2026-02-10 16:24:02 +0530

    business

---
+++ app/business/page.tsx
@@ -0,0 +1,33 @@
+import PrepaidPlansClient from "./PrepaidPlansClient";
+
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+async function getPrepaidPlans() {
+  try {
+    const res = await fetch(
+      `${API_BASE}/api/plans/v1/category/business-plans`,
+      {
+        headers: {
+          Accept: "application/json",
+        },
+        cache: "no-store",
+      }
+    );
+
+    if (!res.ok) {
+      console.error("Failed API status:", res.status);
+      return [];
+    }
+
+    return res.json();
+  } catch (error) {
+    console.error("Server fetch error:", error);
+    return [];
+  }
+}
+
+export default async function PrepaidPage() {
+  const plans = await getPrepaidPlans();
+
+  return <PrepaidPlansClient plans={plans} />;
+}
