299c409003d174573a2bcaca8a03373a65a258dd
sumonklyn <sumonklyn@gmail.com>
2026-02-10 17:01:03 +0530

    activationFees1

--- app/family-plans/page.tsx
+++ app/family-plans/page.tsx
@@ -328,7 +328,7 @@ const faqs = [
         {/* RIGHT IMAGE */}
         <div className="relative w-full h-full rounded-2xl overflow-hidden flex items-center justify-center">
           <Image
-            src="/img/familyFeatures.png"
+            src="/img/familyFeatures.jpeg"
             alt="GoLite family plan"
             fill
             className="object-contain"
