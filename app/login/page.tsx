7146394f04ef800ed4293152d0494029b50dac58
kallolghosal <kghosal@gmain.com>
2026-02-11 11:19:32 +0530

    Social login button icons updated

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -175,6 +175,12 @@ function LoginPageContent() {
                   disabled={loading}
                   className="w-full inline-flex items-center justify-center py-2 px-4 border rounded-md bg-white hover:bg-gray-50"
                 >
+                  <svg width="20" height="20" viewBox="0 0 48 48">
+                    <path fill="#FFC107" d="M43.6 20.5H42V20H24v8h11.3C33.6 32.6 29.2 36 24 36c-6.6 0-12-5.4-12-12s5.4-12 12-12c3 0 5.7 1.1 7.8 2.9l5.7-5.7C34.6 6.1 29.6 4 24 4 12.9 4 4 12.9 4 24s8.9 20 20 20c10 0 19-7 19-20 0-1.3-.1-2.7-.4-3.5z"/>
+                    <path fill="#FF3D00" d="M6.3 14.7l6.6 4.8C14.6 16.1 18.9 12 24 12c3 0 5.7 1.1 7.8 2.9l5.7-5.7C34.6 6.1 29.6 4 24 4 16.3 4 9.6 8.4 6.3 14.7z"/>
+                    <path fill="#4CAF50" d="M24 44c5.2 0 10-2 13.5-5.3l-6.2-5.1C29.3 35.1 26.8 36 24 36c-5.2 0-9.6-3.4-11.2-8.1l-6.5 5C9.5 39.5 16.2 44 24 44z"/>
+                    <path fill="#1976D2" d="M43.6 20.5H42V20H24v8h11.3c-1 2.7-2.9 5-5.3 6.6l6.2 5.1C40.9 36.6 44 30.8 44 24c0-1.3-.1-2.7-.4-3.5z"/>
+                  </svg> &nbsp;
                   Sign in with Google
                 </button>
               )}
@@ -198,6 +204,14 @@ function LoginPageContent() {
                   disabled={loading}
                   className="w-full inline-flex items-center justify-center py-2 px-4 border rounded-md bg-blue-600 text-white hover:bg-blue-700"
                 >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="white"
+                    className="w-5 h-5"
+                  >
+                    <path d="M22 12a10 10 0 10-11.5 9.9v-7h-2.3V12h2.3V9.8c0-2.3 1.4-3.5 3.4-3.5.98 0 2 .18 2 .18v2.2h-1.13c-1.1 0-1.45.69-1.45 1.4V12h2.46l-.39 2.9h-2.07v7A10 10 0 0022 12z"/>
+                  </svg>&nbsp;
                   Sign in with Facebook
                 </button>
               )}
