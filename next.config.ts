3a4120f5f9e1555d141c9271efe55a334378f417
Anowar Sadat <anowar.vista@gmail.com>
2026-03-12 10:21:22 +0530

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: [
-              "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com",
-              "style-src 'self' 'unsafe-inline'",
-              "frame-src 'self' https://js.stripe.com https://hooks.stripe.com",
-              "connect-src 'self' https://api.stripe.com",
-              "img-src 'self' data: https://*.stripe.com",
-            ].join("; "),
-          },
-        ],
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
